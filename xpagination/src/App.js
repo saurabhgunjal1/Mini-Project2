78273fb7fee0e40a97a8463c8ce7ef0d9f77a79c
Saurabh Gunjal <gunjalsaurabh16@gmail.com>
2024-05-12 00:46:23 +0530

    commit5

--- xpagination/src/App.js
+++ xpagination/src/App.js
@@ -1,12 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Pagination from "./pagination";
+
 import Pagination2 from "./pagenation2";
 
 function App() {
   return (
     <div className="App">
-      {/* <Pagination /> */}
       <Pagination2 />
     </div>
   );
